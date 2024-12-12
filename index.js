e47865708de53e9dccdffef3d2e9082d190029ec
Kishu Jaiswal <kishujaiswal007@gmail.com>
2024-12-12 20:01:59 +0530

    .env updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import {connectDB} from "./database/mongoose.js";
 import app from "./app.js";
-dotenv.config({ path: 'D:\kishu\Project_cart\cartbackend\.env' });
+dotenv.config({ path: './.env' });
 connectDB().then(() => {
     app.listen(process.env.PORT||3001, () => {
         console.log("database connected");
